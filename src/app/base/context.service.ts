d38ce9282da427e06027853b509accadf3cca915
Daniele Morosinotto <d.morosinotto@icloud.com>
2019-04-03 11:58:15 +0200

    Fix bind in context.service

--- src/app/base/context.service.ts
+++ src/app/base/context.service.ts
@@ -9,31 +9,35 @@ export class ContextService implements OnDestroy {
   ngOnDestroy(): void {
     this._destroy$.next();
     this._destroy$.complete();
-    //cleanup internals (I don't know if it's safer to execute this in next tick using Promise.resolve().then( ()=>for...)
+    //cleanup internals I don't know if it's safer to execute this in nextTick Promise.resolve().then()
     for (let _key in this._) (this._[_key] as unknown) = undefined;
   }
 
   private _: {
+    //internals use an object map string -> BehaviorSubject (ndr token -> reactive variable)
     [_key: string]: BehaviorSubject<unknown | undefined>;
   } = {};
 
   provide<T>(key: string | InjectionToken<T>, initVal?: T): Observer<T> {
     const _key = key.toString();
     if (!(_key in this._)) this._[_key] = new BehaviorSubject<T | undefined>(initVal);
-    else this._[_key].next(initVal); //if no initVal is passed it emit undefined but it's filtered in the get$ ;-)
+    else this._[_key].next(initVal); //if no initVal is passed it emits undefined but it's filtered
 
+    const _bs = this._[_key];
     return {
-      next: this._[_key].next,
-      error: this._[_key].error,
-      complete: this._[_key].complete
+      next: _bs.next.bind(_bs),
+      error: _bs.error.bind(_bs),
+      complete: _bs.complete.bind(_bs)
     };
   }
 
   get$<T>(key: string | InjectionToken<T>): Observable<T> {
     const _key = key.toString();
+    console.log("EXIST", _key in this._);
     if (!(_key in this._)) this._[_key] = new BehaviorSubject<T | undefined>(undefined);
+
     return (this._[_key].asObservable() as Observable<T>).pipe(
-      filter(t => t !== undefined), //filter the initial undefined, or possible initVal not passed (ndr undefined)
+      filter(t => t !== undefined), //filter the initial undefined, or possible initVal not passed
       takeUntil(this._destroy$)
     );
   }
